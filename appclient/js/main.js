01296be7f3d078a483edfaaf37662dcbc54818a0
Chow22 <asiergalan@gmail.com>
2020-04-02 17:27:03 +0200

    lista de alumnos con json

--- appclient/js/main.js
+++ appclient/js/main.js
@@ -5,9 +5,47 @@ window.addEventListener('load', init());
 function init() {
     console.debug('Document Load and Ready');
     
+    let alumnosJSON = [{
+        "avatar": "img/avatar1.png",
+        "nombre": "Oconnor",
+
+    },
+    {
+        "avatar": "img/avatar2.png",
+        "nombre": "Pepa",
+
+    },
+    {
+        "avatar": "img/avatar3.png",
+        "nombre": "JoseMAri",
+
+    },
+    {
+        "avatar": "img/avatar4.png",
+        "nombre": "JuanCar",
+
+    },
+    {
+        "avatar": "img/avatar5.png",
+        "nombre": "Gustavo",
+
+    },
+    {
+        "avatar": "img/avatar6.png",
+        "nombre": "Enrique",
+
+    },
+    {
+        "avatar": "img/avatar7.png",
+        "nombre": "Maria",
+
+    }];
+
+
+
     alumnos.innerHTML='';
-    for (let i = 1; i < 8; i++) {
-        alumnos.innerHTML += `<li class="list-item"><img src="img/avatar${i}.png" alt="avatar">Alumno ${i}</li><hr>`;
+    for (let i = 0; i < alumnosJSON.length; i++) {
+        alumnos.innerHTML += '<li class="list-item"><img src="'+alumnosJSON[i].avatar+'" alt="avatar">'+alumnosJSON[i].nombre+'</li><hr>';
     }
 
 
