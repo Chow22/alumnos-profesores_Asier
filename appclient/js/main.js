44cae5cfb0dccd00f427476700e563f75e7124c2
Chow22 <asiergalan@gmail.com>
2020-04-02 10:19:39 +0200

    lista dinamica con js index.html

--- appclient/js/main.js
+++ appclient/js/main.js
@@ -1,7 +1,14 @@
 // fichero javascript para app
 
-window.addEventListener('load', init() );
+window.addEventListener('load', init());
 
-function init(){
+function init() {
     console.debug('Document Load and Ready');
+    
+    alumnos.innerHTML='';
+    for (let i = 1; i < 8; i++) {
+        alumnos.innerHTML += `<li class="list-item"><img src="img/avatar${i}.png" alt="avatar">Alumno ${i}</li><hr>`;
+    }
+
+
 }
