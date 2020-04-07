daa10e7869e900231296d2b39017120b00ba96d5
Chow22 <asiergalan@gmail.com>
2020-04-07 15:15:32 +0200

    json desde api

--- appclient/js/main.js
+++ appclient/js/main.js
@@ -1,79 +1,58 @@
 // fichero javascript para app
-const alumnosJSON = [{
-    "avatar": "img/avatar1.png",
-    "nombre": "Oconnor",
-    "sexo": "h"
-
-},
-{
-    "avatar": "img/avatar2.png",
-    "nombre": "Pepa",
-    "sexo": "m"
-
-},
-{
-    "avatar": "img/avatar3.png",
-    "nombre": "JoseMAri",
-    "sexo": "h"
-
-},
-{
-    "avatar": "img/avatar4.png",
-    "nombre": "JuanCar",
-    "sexo": "h"
-
-},
-{
-    "avatar": "img/avatar5.png",
-    "nombre": "Gustavo",
-    "sexo": "h"
-
-},
-{
-    "avatar": "img/avatar6.png",
-    "nombre": "Enrique",
-    "sexo": "h"
-
-},
-{
-    "avatar": "img/avatar7.png",
-    "nombre": "Maria",
-    "sexo": "m"
-
-}];
 
 window.addEventListener('load', init());
-
+let retornoJson;
 function init() {
-    console.debug('Document Load and Ready');
-    pintarLista();
-
-    function pintarLista() {
-        let lista = document.getElementById('alumnos');
-        lista.innerHTML = '';
-        for (let i = 0; i < alumnosJSON.length; i++) {
-            lista.innerHTML += '<li class="list-item"><img src="' + alumnosJSON[i].avatar + '" alt="avatar">' + alumnosJSON[i].nombre + '</li>';
-        }
-    }
+    cargarJSON();
 }
 
-function filtrarSexo() {
-    let selector=document.getElementById('selectorSexo').value;
+function cargarJSON() {
+    url = "http://localhost:8080/apprest/api/personas/";
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+
+            // Json
+            const jsonData = JSON.parse(this.responseText);
+
+            retornoJson = jsonData;
+
+            console.debug('Document Load and Ready');
+            let selectorSexo = document.getElementById("selectorSexo");
+            pintarLista(jsonData);
+            selectorSexo.addEventListener('change', filtrarSexo(jsonData), false);
+        }
+
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+function pintarLista(alumnosJSON) {
+    let lista = document.getElementById('alumnos');
+    lista.innerHTML = '';
+    for (let i = 0; i < alumnosJSON.length; i++) {
+        lista.innerHTML += '<li class="list-item"><img src="img/' + alumnosJSON[i].avatar + '" alt="avatar">' + alumnosJSON[i].nombre + '</li>';
+    }
+};
+
+function filtrarSexo(alumnosJSON) {
+    let selector = document.getElementById('selectorSexo').value;
     alert(selector);
     let personasFiltradas;
-    if(selector=='t'){
-        personasFiltradas=alumnosJSON;
-    }else{
+    if (selector == 't') {
+        personasFiltradas = alumnosJSON;
+    } else {
         personasFiltradas = alumnosJSON.filter(el => el.sexo == selector);
     };
-    
-console.debug(personasFiltradas);
+
+    console.debug(personasFiltradas);
     //pintar la lista filtrada
     let lista = document.getElementById('alumnos');
     lista.innerHTML = '';
     for (let i = 0; i < personasFiltradas.length; i++) {
-        lista.innerHTML += '<li class="list-item"><img src="' + personasFiltradas[i].avatar + '" alt="avatar">' + personasFiltradas[i].nombre + '</li>';
+        lista.innerHTML += '<li class="list-item"><img src="img/' + personasFiltradas[i].avatar + '" alt="avatar">' + personasFiltradas[i].nombre + '</li>';
     }
 
 };
-
