c7000e3f1a117b4c41004f12b5ac581c830787c2
chow22 <asiergalan@gmail.com>
2020-04-14 11:49:21 +0200

    actividad 9 terminar interfaz grafica

--- appclient/js/main.js
+++ appclient/js/main.js
@@ -7,6 +7,9 @@ window.addEventListener('load', init());
 function init() {
     console.debug('Document Load and Ready');
     listener();
+    initGallery();
+
+
 
 
     const promesa = ajax("GET", endpoint, undefined);
@@ -95,7 +98,7 @@ function eliminar(indice) {
 
 function seleccionar(indice) {
 
-    let personaSeleccionada = { "id": personas.length+1, "nombre": "sin nombre" };
+    let personaSeleccionada = { "id": personas.length + 1, "nombre": "sin nombre" };
 
     if (indice != '*') {
         personaSeleccionada = personas[indice];
@@ -106,6 +109,21 @@ function seleccionar(indice) {
     //rellernar formulario
     document.getElementById('inputId').value = personaSeleccionada.id;
     document.getElementById('inputNombre').value = personaSeleccionada.nombre;
+    document.getElementById('inputAvatar').value = personaSeleccionada.avatar;
+
+    const sexo = personaSeleccionada.sexo;
+    let checkHombre = document.getElementById('sexoh');
+    let checkMujer = document.getElementById('sexom');
+
+    if ( sexo == "h"){
+        checkHombre.checked = 'checked';
+        checkMujer.checked = '';
+
+    }else{
+        checkHombre.checked = '';
+        checkMujer.checked = 'checked';
+    }
+
 
 }
 
@@ -114,28 +132,56 @@ function guardar() {
     console.trace('click guardar');
     let id = document.getElementById('inputId').value;
     let nombre = document.getElementById('inputNombre').value;
+    const avatar = document.getElementById('inputAvatar').value;
+    const sexo = document.getElementById('inputSexo').value;
 
     personaSeleccionada = {
         "id": id,
         "nombre": nombre,
-        "avatar": "avatar7.png"
+        "avatar": avatar,
+        "sexo": sexo
     };
-    console.info( personas.some(el => el.id == 2 ));
+    console.info(personas.some(el => el.id == 2));
 
-    if (personas.some(el => el.id == id )){
+    if (personas.some(el => el.id == id)) {
         personas.map(function (dato) {
             if (dato.id == id) {
                 dato.nombre = nombre;
+                dato.avatar = avatar;
+                dato.sexo = sexo;
             }
         })
-    }else{
+    } else {
         personas.push(personaSeleccionada);
     }
     console.debug('persona a guardar %o', personaSeleccionada);
 
     //TODO llamar servicio rest
-
-  
     pintarLista(personas);
+
+}
+
+
+function initGallery() {
+    let divGallery = document.getElementById('gallery');
+    for (let i = 1; i <= 7; i++) {
+        divGallery.innerHTML += `<img onclick="selectAvatar(event)" 
+                                      class="avatar" 
+                                      data-path="img/avatar${i}.png"
+                                      src="img/avatar${i}.png">`;
+    }
+}
+
+function selectAvatar(evento) {
+    console.trace('click avatar');
+    const avatares = document.querySelectorAll('#gallery img');
+    //eliminamos la clases 'selected' a todas las imagenes del div#gallery
+    avatares.forEach(el => el.classList.remove('selected'));
+    // ponemos clase 'selected' a la imagen que hemos hecho click ( evento.target )
+    evento.target.classList.add('selected');
+
+    let iAvatar = document.getElementById('inputAvatar');
+    //@see: https://developer.mozilla.org/es/docs/Learn/HTML/como/Usando_atributos_de_datos
+    iAvatar.value = evento.target.dataset.path;
 
 }
